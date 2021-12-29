1048af0981fded583ae149213b0aec22db3d68d4
Büşra Sarı <e170503019@stud.tau.edu.tr>
2021-12-29 23:05:30 +0300

    anlamsız key bug fixed

--- Entities/Concrete/DraftJob.cs
+++ Entities/Concrete/DraftJob.cs
@@ -1,6 +1,7 @@
 ﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace Entities.Concrete
 {
     public class DraftJob : IEntity
     {
+        [Key]
         public int JobId { get; set; } //PK
         public int CompanyId { get; set; } //FK
         public String JobName { get; set; }
