ad32009e101a6b02ce36f9c9e604bb2cfecabf12
tugbaoguncmert <61146627+tugbaoguncmert@users.noreply.github.com>
2022-01-03 22:43:07 +0300

    Download CV
    Download CV

--- WebApplication1/Controllers/CurriculumVitaesController.cs
+++ WebApplication1/Controllers/CurriculumVitaesController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -52,6 +53,43 @@ namespace WebAPI.Controllers
                 return Ok(result);
             }
             return BadRequest(result);
+        }
+
+        //[HttpPost("download")]
+        //public async Task<IActionResult> DownloadAsync()
+        //{
+        //    var path = @"C:\Users\Tuğba Öğünçmert\Documents\GitHub\Expert-In-Backend-Release\WebApplication1\wwwroot\Uploads\CurriculumVitaes\Tugba_Oguncmert_CV.pdf";
+        //    var memory = new MemoryStream();
+        //    using (var stream = new FileStream(path, FileMode.Open))
+        //    {
+        //        await stream.CopyToAsync(memory);
+        //    }
+        //    memory.Position = 0;
+        //    var ext = Path.GetExtension(path).ToLowerInvariant();
+        //    return File(memory, GetMimeTypes()[ext], Path.GetFileName(path));
+        //}
+
+        [HttpPost("download")]
+        public async Task<IActionResult> DownloadAsync(string curriculumVitaePath)//önemli olan sadece path strinf path de yazabilirsiniz.
+        {
+            //var path = @"C:\Users\Tuğba Öğünçmert\Documents\GitHub\Expert-In-Backend-Release\WebApplication1\wwwroot\Uploads\CurriculumVitaes\Tugba_Oguncmert_CV.pdf";
+            var memory = new MemoryStream();
+            using (var stream = new FileStream(curriculumVitaePath, FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+            var ext = Path.GetExtension(curriculumVitaePath).ToLowerInvariant();
+            return File(memory, GetMimeTypes()[ext], Path.GetFileName(curriculumVitaePath));
+        }
+
+        private Dictionary<string,string> GetMimeTypes()
+        {
+            return new Dictionary<string, string>
+            {
+                {".pdf","application/pdf" },
+                {".png","image/png" }
+            };
         }
     }
 }
